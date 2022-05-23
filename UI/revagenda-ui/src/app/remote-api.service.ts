1590b0bc3e3f131602c2c5c7b170f8d0ee660681
Kyle <Kyle.Plummer@revature.com>
2022-05-23 16:01:36 -0500

    fixing buildspec copy operation command

--- UI/revagenda-ui/src/app/remote-api.service.ts
+++ UI/revagenda-ui/src/app/remote-api.service.ts
@@ -8,7 +8,7 @@ import { catchError, Observable, retry, throwError } from 'rxjs';
 export class RemoteApiService {
 
   //baseUrl: string = "http://localhost:8080/tasks/new";
-  baseUrl: string = "http://revagenda-jpa-ui.s3-website-us-east-1.amazonaws.com/tasks/new"
+  baseUrl: string = "Revagendakp-env.eba-mkadq2gc.us-east-1.elasticbeanstalk.com/tasks/new"
 
   constructor(private http: HttpClient) { }
 
@@ -28,7 +28,7 @@ export class RemoteApiService {
 
   test(): Observable<any> {
     console.log("test");
-    return this.http.get<object>("http://revagenda-jpa-ui.s3-website-us-east-1.amazonaws.com/health/ping", { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) })
+    return this.http.get<object>("Revagendakp-env.eba-mkadq2gc.us-east-1.elasticbeanstalk.com/health/ping", { headers: new HttpHeaders({ 'Content-Type': 'application/json' }) })
       .pipe(
         retry(1),
         catchError(this.errorHandler)
@@ -36,8 +36,8 @@ export class RemoteApiService {
   }
 
   testTaskGet(): Observable<any> {
-    return this.http.get<object>("http://revagenda-jpa-ui.s3-website-us-east-1.amazonaws.com/tasks/1", 
-    {observe: 'response', 
+    return this.http.get<object>("Revagendakp-env.eba-mkadq2gc.us-east-1.elasticbeanstalk.com/tasks/1",
+    {observe: 'response',
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }) })
       .pipe(
         retry(1),
